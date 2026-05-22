bb0a86faa63ea2ecd88322bea5209ce4713f2b6b
Apart004 <jagdhariansh2004@gmail.com>
2026-05-22 18:49:26 +0530

    feat: implemented malicious IP normalization

--- scripts/threat_feed.py
+++ scripts/threat_feed.py
@@ -5,10 +5,17 @@ url = "https://feodotracker.abuse.ch/downloads/ipblocklist.txt"
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.text
+    data = response.text.splitlines()
+
+    clean_ips = []
+
+    for line in data:
+        if not line.startswith("#") and line.strip() != "":
+            clean_ips.append(line)
 
     with open("data/malicious_ips.txt", "w") as file:
-        file.write(data)
+        for ip in clean_ips:
+            file.write(ip + "\n")
 
     print("Threat feed downloaded successfully.")
 
